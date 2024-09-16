c1f29a6659a24815f094eafde31fd1a6406b3e04
TinaD1906 <dotin87@gmail.com>
2024-09-16 10:16:04 -0700

    save

--- Problem1.java
+++ Problem1.java
@@ -2,6 +2,11 @@ public class Problem1
 {
   public static void main(String[] args)
   {
-    // insert code here
+    int num = 5;
+    int den = 20;
+    double dec = (double) num / den;
+    System.out.println("numerator: " + num);
+    System.out.println("denominator: " + den);
+    System.out.println("The decimal value is: " + dec);
   }
 }
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,12 @@
-
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    double x = 57.3934;
+    double y = 22.5211;
+    int numx = (int)(x + 0.5);
+    int numy = (int)(y + 0.5);
+    int add = numx + numy;
+    System.out.println("Answer: " + numx + " + " + numy + " = " + add);
+  }
+}
