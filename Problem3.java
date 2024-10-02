6abd4d366314894306a396c3e01b6b0007850c5e
TinaD1906 <dotina87@gmail.com>
2024-10-02 10:22:20 -0700

    urmother

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,16 @@
 
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    double num1 = 67.3425;
+    System.out.println(num1*10 % 10);
+    int dec1 = (int) (num1*10 % 10);
+    int dec2 = (int) (num1*10*10 % 10);
+    int dec3 = (int) (num1*10*10*10 % 10);
+    System.out.println( dec1 + " " + dec2 + " " + dec3);
+    //System.out.println(num1/10 % 10);
+    //System.out.println(num1/10/10 % 10);
+    //System.out.println("Answer: " + numx + " + " + numy + " = " + add);
+  }
+}
